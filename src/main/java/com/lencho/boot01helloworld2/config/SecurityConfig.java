0fa68268e809ee226e8e945f2bd2559bc1caec21
Lencho2010 <335999737@qq.com>
2021-07-24 10:58:15 +0800

    spring security 代码提交

---
+++ src/main/java/com/lencho/boot01helloworld2/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.lencho.boot01helloworld2.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Lencho
+ * @create 2021-07-24 10:46
+ */
+@Slf4j
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = passwordEncoder.encode("123456");
+        auth.inMemoryAuthentication().withUser("lucy").password(password).roles("admin");
+        log.info("123");
+    }
+
+    @Bean
+    public PasswordEncoder password(){
+        return new BCryptPasswordEncoder();
+    }
+}
